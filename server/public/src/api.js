d62876e07b43c3821d5578461ae97bfdd699b3ef
tizzypenguin <tizzpenguin@gmail.com>
2019-06-19 00:02:01 +0900

    테스트용 db 작성 및 리스트 조회 구조 구성

--- server/public/src/api.js
+++ server/public/src/api.js
@@ -13,21 +13,42 @@ let db = new sqlite3.Database(dbPath, (err) => {
 	} 
 })
 
-router.get('/', function(req, res, next) {
-	let arr = [];
+let settingList = function (siteRows, matserRows, detailRows) {
+	let returnArr = [];
+	
+}
+
+router.get('/list', function(req, res, next) {
+	let siteRows, masterRows, detailRows = {};
+
 	const params = [];
 	db.all("SELECT NO_SITE AS no, NAME AS name, URL AS url, ASIS AS asis, TOBE AS tobe, YN_CHANGE AS yn_change FROM TBL_SITE", params, (err, rows) => {
 		if(err) {
 			res.status(400).json({"error":err.message});
 			return;
 		} else {
-			// res.json({
-			// 	"message":"success",
-			// 	"data":rows
-			// })
-			res.send(rows);
+			// res.send(rows);
+			siteRows = rows;
+			db.all("SELECT NO_SITE AS no_site, NO_MASTER AS no, NAME AS name, DURATION AS duration, TAG AS tag FROM TBL_MASTER", params, (err, rows) => {
+				if(err) {
+					res.status(400).json({"error":err.message});
+					return;
+				} else {
+					masterRows = rows;
+					db.all("SELECT NO_MATSER AS no_matser, NO_DETAIl AS no, REG_DT_FROM AS dt_from, REG_DT_TO AS dt_to, DT_EXAM AS dt_exam FROM TBL_DETAIL", params, (err, rows) => {
+						if(err) {
+							res.status(400).json({"error":err.message});
+							return;
+						} else {
+							detailRows = rows;
+							settingList(siteRows, masterRows, detailRows)
+						}
+					})
+				}
+			});
 		}
 	})
 });
+
 
 module.exports = router;
