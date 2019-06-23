1b7d0110beb66761516ba38bf662000231d0b2d1
tizzypenguin <tizzpenguin@gmail.com>
2019-06-24 00:07:46 +0900

    데이터 조합 구현중

--- server/public/src/api.js
+++ server/public/src/api.js
@@ -15,7 +15,26 @@ let db = new sqlite3.Database(dbPath, (err) => {
 
 let settingList = function (siteRows, masterRows, detailRows) {
 	let returnArr = [];
+
 	console.log(siteRows, masterRows, detailRows);
+	for(var site of siteRows) {
+		returnArr.push(site);
+	}
+	for(var master of masterRows) {
+		var no_site = master.no_site;
+		var site = returnArr.filter(site => site.no = no_site);
+		if(!site || site.length != 1 ) {
+			return null;
+		} else {
+			if(!site[0].arr) {
+				site[0].arr = [];
+				site[0].arr.push(master);
+			} else {
+				site[0].arr.push(master);
+			}
+		}
+	}
+	
 }
 
 router.get('/list', function(req, res, next) {
@@ -23,6 +42,7 @@ router.get('/list', function(req, res, next) {
 
 	const params = [];
 	db.all("SELECT NO_SITE AS no, NAME AS name, URL AS url, ASIS AS asis, TOBE AS tobe, YN_CHANGE AS yn_change FROM TBL_SITE", params, (err, rows) => {
+		console.log("first:",rows);
 		if(err) {
 			res.status(400).json({"error":err.message});
 			return;
@@ -30,18 +50,26 @@ router.get('/list', function(req, res, next) {
 			// res.send(rows);
 			siteRows = rows;
 			db.all("SELECT NO_SITE AS no_site, NO_MASTER AS no, NAME AS name, DURATION AS duration, TAG AS tag FROM TBL_MASTER", params, (err, rows) => {
+				console.log("second:",rows);
 				if(err) {
 					res.status(400).json({"error":err.message});
 					return;
 				} else {
 					masterRows = rows;
-					db.all("SELECT NO_MATSER AS no_matser, NO_DETAIl AS no, REG_DT_FROM AS dt_from, REG_DT_TO AS dt_to, DT_EXAM AS dt_exam FROM TBL_DETAIL", params, (err, rows) => {
+					db.all("SELECT NO_MASTER AS no_matser, NO_DETAIL AS no, REG_DT_FROM AS dt_from, REG_DT_TO AS dt_to, DT_EXAM AS dt_exam, NO_NEXT AS no_next FROM TBL_DETAIL", params, (err, rows) => {
+						console.log("third:",rows);
 						if(err) {
 							res.status(400).json({"error":err.message});
 							return;
 						} else {
 							detailRows = rows;
-							settingList(siteRows, masterRows, detailRows);
+							let settingList = settingList(siteRows, masterRows, detailRows);
+							if(settingList) {
+								res.send(settingList);
+							} else {
+								res.status(400).json({"error":"자료 구성 중 에러 발생"});
+							}
+							
 						}
 					})
 				}
